6827f11be09b3b5e2794db7693a0a0e6f03b972d
Cristian <cris.esroj@gmail.com>
2020-09-13 20:32:19 -0300

    implement Admin and you method

--- src/main/java/ar/edu/grupoi/backend/DesappBackend/model/project/Project.java
+++ src/main/java/ar/edu/grupoi/backend/DesappBackend/model/project/Project.java
@@ -1,5 +1,9 @@
 package ar.edu.grupoi.backend.DesappBackend.model.project;
 
+import java.util.List;
+
+import ar.edu.grupoi.backend.DesappBackend.model.user.Donor;
+
 public class Project {
 
 	public Location getLocation() {
@@ -7,7 +11,32 @@ public class Project {
 		return null;
 	}
 
-	public String getNombre() {
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public float getCumulativePercentage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getCollection() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int moneyRequerid() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setState(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Donor> getDonors() {
 		// TODO Auto-generated method stub
 		return null;
 	}
