c4a19a4bd7f92d3ac0ac0501b3e3d098780f6279
Cristian <cris.esroj@gmail.com>
2020-09-22 13:11:28 -0300

    refactor system and test admin

--- src/main/java/ar/edu/grupoi/backend/desappbackend/model/project/System.java
+++ src/main/java/ar/edu/grupoi/backend/desappbackend/model/project/System.java
@@ -7,32 +7,41 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-
 public class System {
 
-    public final ArrayList<Donation> donations;
-    public final ArrayList<Location> locations;
-    public final ArrayList<Project> projects;
-    public final ArrayList<Donor> donors;
-    public final Admin admin;
+	public final ArrayList<Donation> donations;
+	public final ArrayList<Location> locations;
+	public final ArrayList<Project> projects;
+	public final ArrayList<Donor> donors;
+	public final Admin admin;
 
-    public System(Admin admin) {
-        this.donations = new ArrayList<>();
-        this.locations = new ArrayList<>();
-        this.projects  = new ArrayList<>();
-        this.donors    = new ArrayList<>();
-        this.admin     = admin;
-    }
+	public System(Admin admin) {
+		this.donations = new ArrayList<>();
+		this.locations = new ArrayList<>();
+		this.projects = new ArrayList<>();
+		this.donors = new ArrayList<>();
+		this.admin = admin;
+	}
 
-    public void addDonation(Donation donation) { donations.add(donation); }
+	public void addDonation(Donation donation) {
+		donations.add(donation);
+	}
 
-    public void addLocation(Location location) { locations.add(location); }
+	public void addLocation(Location location) {
+		locations.add(location);
+	}
 
-    public void addProject(Project project) { projects.add(project); }
+	public void addProject(Project project) {
+		projects.add(project);
+	}
 
-    public void addDonor(Donor donor) { donors.add(donor); }
+	public void addDonor(Donor donor) {
+		donors.add(donor);
+	}
 
-    public Admin getAdmin() { return admin; }
+	public Admin getAdmin() {
+		return admin;
+	}
 
 	public Integer accumulatedPoints(Donor aDonor) {
 		ArrayList<Donation> donationsDonor = donationOfDonor(aDonor);
@@ -40,23 +49,18 @@ public class System {
 	}
 
 	private int totalPointsDonations(ArrayList<Donation> donationsDonor) {
-		return donationsDonor.stream()
-				.mapToInt(Donation::getPoints)
-				.sum();
+		return donationsDonor.stream().mapToInt(Donation::getPoints).sum();
 	}
 
 	private int totalDonationsMoth(ArrayList<Donation> donationsDonor) {
 		int totalDonationsMoth = (int) donationsDonor.stream()
-				.filter(donation -> donation.getDate().getMonth() == LocalDate.now().getMonth())
-				.count();
+				.filter(donation -> donation.getDate().getMonth() == LocalDate.now().getMonth()).count();
 		return totalDonationsMoth >= 2 ? 500 : 0;
 	}
-	
+
 	private ArrayList<Donation> donationOfDonor(Donor aDonor) {
-		return donations.stream()
-				.filter(donation -> donation.getNickname().equals(aDonor.getNickname()))
-				.collect(Collectors
-		        .toCollection(ArrayList::new));
+		return donations.stream().filter(donation -> donation.getNickname().equals(aDonor.getNickname()))
+				.collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	public void donate(Donor aDonor, Project project, Integer amount, String comment) {
@@ -68,7 +72,7 @@ public class System {
 		admin.finishCollection(project);
 	}
 
-	public void notiftNews(Project project) {
+	public void notifyNews(Project project) {
 		ArrayList<Donor> donorsproject = findDonorsProject(project);
 		admin.notifyNews(project, donorsproject);
 	}
@@ -77,12 +81,9 @@ public class System {
 		ArrayList<String> nicknames = findDonorsNicknameProject(project);
 		return findDonorsNickname(nicknames);
 	}
-	
+
 	private ArrayList<Donor> findDonorsNickname(ArrayList<String> nicknames) {
-		return nicknames.stream()
-				.map(this::findDonorNickname)
-				.collect(Collectors
-                .toCollection(ArrayList::new));
+		return nicknames.stream().map(this::findDonorNickname).collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	private ArrayList<String> findDonorsNicknameProject(Project project) {
@@ -91,23 +92,16 @@ public class System {
 	}
 
 	private ArrayList<String> getNicknamesDonations(ArrayList<Donation> donationsProject) {
-		return donationsProject.stream()
-				.map(Donation::getNickname)
-				.collect(Collectors
-                .toCollection(ArrayList::new));
+		return donationsProject.stream().map(Donation::getNickname).collect(Collectors.toCollection(ArrayList::new));
 	}
 
-	private  ArrayList<Donation> findDonationProject(String name) {
-		return donations.stream()
-				.filter(donation -> donation.getNameProject().equals(name))
-				.collect(Collectors
-		        .toCollection(ArrayList::new));
+	private ArrayList<Donation> findDonationProject(String name) {
+		return donations.stream().filter(donation -> donation.getNameProject().equals(name))
+				.collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	private Donor findDonorNickname(String apodo) {
-		return donors.stream()
-				.filter(donor -> donor.getNickname().equals(apodo))
-				.collect(Collectors.toList()).get(0);
+		return donors.stream().filter(donor -> donor.getNickname().equals(apodo)).collect(Collectors.toList()).get(0);
 	}
 
 }
