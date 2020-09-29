aac84bb46768eef35928f015775ea3f747bcb7db
Cristian <cris.esroj@gmail.com>
2020-09-29 20:40:58 -0300

    implementation donor builder and memory base

--- src/main/java/ar/edu/grupoi/backend/desappbackend/DesappBackendApplication.java
+++ src/main/java/ar/edu/grupoi/backend/desappbackend/DesappBackendApplication.java
@@ -1,7 +1,13 @@
 package ar.edu.grupoi.backend.desappbackend;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ar.edu.grupoi.backend.desappbackend.model.user.Donor;
+import ar.edu.grupoi.backend.desappbackend.repositories.DonorRepository;
+import ar.edu.grupoi.backend.desappbackend.service.builder.DonorBuilder;
 
 @SpringBootApplication
 public class DesappBackendApplication {
@@ -10,4 +16,21 @@ public class DesappBackendApplication {
 		SpringApplication.run(DesappBackendApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner applicationRunner(DonorRepository donorRepository) {
+		return args -> {
+			Donor cris = DonorBuilder.whitName("Cristian")
+					.whitMail("cris@mail.com")
+					.whitPassword("cris123")
+					.withNickname("Cris").builder();
+			
+			Donor jony = DonorBuilder.whitName("Jonathan")
+					.whitMail("jony@mail.com")
+					.whitPassword("jony213")
+					.withNickname("Jony").builder();
+			donorRepository.save(cris);
+			donorRepository.save(jony);
+		};
+
+	}
 }
