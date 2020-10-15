8d3c6078cb136ddc956ccd9e51961defd0c740d9
Cristian <cris.esroj@gmail.com>
2020-10-14 21:32:08 -0300

    modification top10 donation get

--- src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/AdminController.java
+++ src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +43,7 @@ public class AdminController {
 	}
 	
 	@CrossOrigin
-	@PostMapping("/top10")
+	@GetMapping("/top10")
 	public void top10() {
 		adminService.top10Donations();
 	}
