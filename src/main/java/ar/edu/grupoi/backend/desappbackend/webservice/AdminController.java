0afdf8335fc60368b4787c1a0ebd9ddac20c42d9
Cristian <cris.esroj@gmail.com>
2020-10-14 21:53:21 -0300

    modification admin controller top10 get

--- src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/AdminController.java
+++ src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,7 +44,7 @@ public class AdminController {
 	}
 	
 	@CrossOrigin
-	@PostMapping("/top10_donations")
+	@GetMapping("/top10_donations")
 	public void top10() {
 		adminService.top10Donations();
 	}
