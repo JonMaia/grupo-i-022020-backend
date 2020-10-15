ffc9fbaf73e79d8ea41cb0005b874793572fb36d
Cristian <cris.esroj@gmail.com>
2020-10-14 21:26:18 -0300

    modification api rest admin

--- src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/AdminController.java
+++ src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/AdminController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,14 +36,14 @@ public class AdminController {
 	}
 	
 	@CrossOrigin
-	@PutMapping("/finish_collection")
+	@PostMapping("/finish_collection")
 	public Project finishCollection(@RequestBody DtoProject project) {
 		return adminService.finishCollection(project);
 	}
 	
 	@CrossOrigin
-	@GetMapping("/top10_donations")
-	public void top10Donationes() {
+	@PostMapping("/top10_donations")
+	public void top10Donations() {
 		adminService.top10Donations();
 	}
 }
