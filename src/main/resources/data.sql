557bdb5b71e2dc2fe5d9504b5858a68f08edd227
Cristian <cris.esroj@gmail.com>
2020-11-05 17:24:41 -0300

    implementation data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO ADMIN (ID, MAIL, NAME, PASSWORD)
+VALUES
+(1, 'admin@gmail.com', 'Admin', 'admin123');
+
+INSERT INTO DONOR (ID, MAIL, NAME, PASSWORD, NICKNAME)
+VALUES
+(2, 'cris.esroj@gmail.com', 'Cristian', 'cris123', 'Cris'),
+(3, 'jony_wilde05@hotmail.com', 'Jonathan', 'jony213', 'Jony');
