ec34cc6f623027611f5d445ba397bbd589bac22c
Cristian <cris.esroj@gmail.com>
2020-11-24 00:38:25 -0300

    modification send mail

--- src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/scheduled/ScheduledMail.java
+++ src/main/java/ar/edu/grupoi/backend/desappbackend/webservice/scheduled/ScheduledMail.java
@@ -3,6 +3,7 @@ package ar.edu.grupoi.backend.desappbackend.webservice.scheduled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.grupoi.backend.desappbackend.service.AdminService;
 
@@ -12,7 +13,8 @@ public class ScheduledMail {
 	@Autowired
 	private AdminService adminService;
 	
-	@Scheduled(cron = "0 30 0 * * *")
+	@Transactional
+	@Scheduled(cron = "0 50 0 * * *")
 	public void sendDailyEmail() {
 		adminService.top10Donations();
 		adminService.top10Locations();
